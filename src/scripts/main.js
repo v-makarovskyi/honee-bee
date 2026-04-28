f4c2d0395fe628b4d4780a598c50e126a06ac88b
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2026-04-28 20:01:27 +0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,14 @@ const overlay = document.querySelector(".overlay");
 const gamburgerBtn = document.querySelector(".gamburger-button");
 const closeBtn = document.querySelector(".close-button");
 
-gamburgerBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("mobile-menu--open");
-  overlay.classList.add("overlay--active");
-});
+window.addEventListener("DOMContentLoaded", () => {
+  gamburgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("mobile-menu--open");
+    overlay.classList.add("overlay--active");
+  });
 
-closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("mobile-menu--open");
-  overlay.classList.remove("overlay--active");
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu--open");
+    overlay.classList.remove("overlay--active");
+  });
 });
